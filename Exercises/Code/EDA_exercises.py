32074b628c087c85905299551c86fbdab7548c38
jsbarbery <168651533+jsbarbery@users.noreply.github.com>
2024-05-13 14:52:39 -0500

    Add files via upload

---
+++ Exercises/Code/EDA_exercises.py
@@ -0,0 +1,69 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+# Load data from a text file
+
+df_students = pd.read_csv(r'C:\Users\Luis Villamar\Desktop\John B\Microsoft Learn\Misc\grades.csv',delimiter=',',header='infer')
+
+# Remove any rows with missing data
+df_students = df_students.dropna(axis=0, how='any')
+
+# Calculate who passed, assuming '60' is the grade needed to pass
+passes  = pd.Series(df_students['Grade'] >= 60)
+
+# Save who passed to the Pandas dataframe
+df_students = pd.concat([df_students, passes.rename("Pass")], axis=1)
+
+
+# Print the result out into this notebook
+print(df_students )
+'''''
+# Create a bar plot of name vs grade
+plt.bar(x=df_students.Name, height=df_students.Grade, color='orange')
+
+# Customize the chart
+plt.title('Student Grades')
+plt.xlabel('Student')
+plt.ylabel('Grade')
+plt.grid(color='#95a5a6', linestyle='--', linewidth=2, axis='y', alpha=0.7)
+plt.xticks(rotation=90)
+
+# Display the plot
+plt.show()'''
+
+# Create a figure for 2 subplots (1 row, 2 columns)
+fig, ax = plt.subplots(1, 2, figsize = (10,4))
+
+# Create a bar plot of name vs grade on the first axis
+ax[0].bar(x=df_students.Name, height=df_students.Grade, color='orange')
+ax[0].set_title('Grades')
+ax[0].set_xticklabels(df_students.Name, rotation=90)
+
+# Create a pie chart of pass counts on the second axis
+pass_counts = df_students['Pass'].value_counts()
+ax[1].pie(pass_counts, labels=pass_counts)
+ax[1].set_title('Passing Grades')
+ax[1].legend(pass_counts.keys().tolist())
+
+# Add a title to the Figure
+fig.suptitle('Student Data')
+
+# Show the figure
+plt.show()
+
+# Get the variable to examine
+var_data = df_students['Grade']
+
+# Create a Figure
+fig = plt.figure(figsize=(10,4))
+
+# Plot a histogram
+plt.hist(var_data)
+
+# Add titles and labels
+plt.title('Data Distribution')
+plt.xlabel('Value')
+plt.ylabel('Frequency')
+
+# Show the figure
+fig.show()
